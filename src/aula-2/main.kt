60a8cd0de5fe65ad6e344e06a22d148b8f9bd421
pmagalhaes2 <paath.magalhaes2@gmail.com>
2022-04-07 15:52:04 -0300

    Inserindo estruturas de repetição e condicionais

--- src/aula-2/main.kt
+++ src/aula-2/main.kt
@@ -3,17 +3,47 @@ package `aula-2`
 fun main() {
     println("Bem vindo ao Bytebank!")
 
-    val titular = "Patricia"
-    val nroConta = 1000
-    var saldoConta = 0.0
-//    saldoConta = 100 + 2.0
-//    saldoConta += 200
+    loop@ for (i in 1..100) {
+        println("i $i")
+        for (j in 1..100) {
+            println("j $j")
+            if (j == 5) break@loop
+        }
+    }
 
-    println("Títular da conta: $titular")
-    println("Número da conta: $nroConta")
-    println("Saldo da conta: $saldoConta")
+//    var i = 0
+//    while (i < 5) {
+//        val titular: String = "Patricia $i"
+//        val nroConta: Int = 1000 + i
+//        var saldoConta = i + 10.0
+//        i++
+//
+//        println("Títular da conta: $titular")
+//        println("Número da conta: $nroConta")
+//        println("Saldo da conta: $saldoConta")
+//        println("")
+//    }
 
+    for (i in 5 downTo 1) {
 
+        if (i == 4) {
+            continue
+        }
+
+        val titular: String = "Patricia $i"
+        val nroConta: Int = 1000 + i
+        var saldoConta = i + 10.0
+
+        println("Títular da conta: $titular")
+        println("Número da conta: $nroConta")
+        println("Saldo da conta: $saldoConta")
+        println("")
+
+        verificaSaldo(saldoConta)
+    }
+}
+
+fun verificaSaldo(saldoConta: Double) {
 //    if(saldoConta > 0.0) {
 //        println("Conta com saldo positivo")
 //    } else if (saldoConta == 0.0) {
@@ -21,10 +51,9 @@ fun main() {
 //    } else {
 //        println("Conta negativa")
 //    }
-
     when {
         saldoConta > 0.0 -> println("Conta com saldo positivo")
         saldoConta == 0.0 -> println("Conta neutra")
-        else -> println("Conta negativa")
+        else -> println("Conta com saldo negativo")
     }
 }
