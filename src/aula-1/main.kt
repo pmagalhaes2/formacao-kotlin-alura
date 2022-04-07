c3d42bcd6f6767fbcf30c80187436db0144dadb9
pmagalhaes2 <paath.magalhaes2@gmail.com>
2022-04-06 22:26:33 -0300

    Inserindo arquivo aula 1

---
+++ src/aula-1/main.kt
@@ -0,0 +1,13 @@
+fun main() {
+    println("Bem vindo ao Bytebank!")
+
+    val titular = "Patricia"
+    val nroConta = 1000
+    var saldoConta = 0.0
+    saldoConta = 100 + 2.0
+    saldoConta += 200
+
+    println("Títular da conta: $titular")
+    println("Número da conta: $nroConta")
+    println("Saldo da conta: $saldoConta")
+}
