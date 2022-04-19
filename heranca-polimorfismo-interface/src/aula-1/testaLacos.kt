6b90ee888091ffaf20f3658b30986cd6b589cc3e
pmagalhaes2 <paath.magalhaes2@gmail.com>
2022-04-19 11:39:45 -0300

    Criação de novas classes

---
+++ heranca-polimorfismo-interface/src/aula-1/testaLacos.kt
@@ -0,0 +1,14 @@
+fun testaLacos() {
+    var i = 0
+    while (i < 5) {
+        val titular: String = "Patricia $i"
+        val nroConta: Int = 1000 + i
+        var saldoConta = i + 10.0
+        i++
+
+        println("Títular da conta: $titular")
+        println("Número da conta: $nroConta")
+        println("Saldo da conta: $saldoConta")
+        println("")
+    }
+}
