32a57df8aaa7b66df78801c8dc219e4a82c69648
pmagalhaes2 <paath.magalhaes2@gmail.com>
2022-04-20 10:00:12 -0300

    Criação das classes Gerente e Funcionário

---
+++ heranca-polimorfismo-interface/src/aula-1/Gerente.kt
@@ -0,0 +1,20 @@
+package `aula-1`
+
+class Gerente(
+    val nome: String,
+    val cpf: String,
+    val salario: Double,
+    val senha: Int
+) {
+    fun bonificacao(): Double {
+        return salario * 0.2
+    }
+
+    fun autentica(senha: Int): Boolean {
+        if (this.senha == senha) {
+            return true
+        }
+        return false
+    }
+
+}
--- heranca-polimorfismo-interface/src/aula-1/main.kt
+++ heranca-polimorfismo-interface/src/aula-1/main.kt
@@ -2,4 +2,34 @@ package `aula-1`
 
 fun main() {
     println("Bem vindo ao Bytebank!")
+
+    val patricia = Funcionario(
+        nome = "Patricia",
+        cpf = "123.456.789-00",
+        salario = 2000.0
+    )
+
+    println("Nome = ${patricia.nome}")
+    println("CPF = ${patricia.cpf}")
+    println("Salário = ${patricia.salario}")
+    println("Bonificação = ${patricia.bonificacao()}")
+
+
+    val pedro = Gerente(
+        nome = "Pedro",
+        cpf = "124.578.878-11",
+        salario = 3000.0,
+        senha = 1234
+    )
+
+    println("Nome = ${pedro.nome}")
+    println("CPF = ${pedro.cpf}")
+    println("Salário = ${pedro.salario}")
+    println("Bonificação = ${pedro.bonificacao()}")
+
+    if (pedro.autentica(123)) {
+        println("Autenticado com sucesso!")
+    } else {
+        println("Falha na autenticação")
+    }
 }
