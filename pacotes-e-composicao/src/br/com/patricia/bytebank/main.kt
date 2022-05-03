d944c546d2f5f904e49b791e067071d6b1187aa7
pmagalhaes2 <paath.magalhaes2@gmail.com>
2022-05-03 17:52:51 -0300

    Sobrescrevendo o método toString

--- pacotes-e-composicao/src/br/com/patricia/bytebank/main.kt
+++ pacotes-e-composicao/src/br/com/patricia/bytebank/main.kt
@@ -1,5 +1,13 @@
-import br.com.patricia.bytebank.test.testaAutenticacaoTiposDiferentes
+import br.com.patricia.bytebank.model.Endereco
 
 fun main() {
-    testaAutenticacaoTiposDiferentes()
+    val endereco = Endereco(logradouro = "Rua Vergueiro", numero = 1000)
+    val enderecoNovo = Endereco(bairro = "Santa Cruz")
+    println(endereco.equals(enderecoNovo))
+
+    println(endereco.hashCode())
+    println(enderecoNovo.hashCode())
+
+    println(endereco)
+    println(enderecoNovo)
 }
--- pacotes-e-composicao/src/br/com/patricia/bytebank/model/Endereco.kt
+++ pacotes-e-composicao/src/br/com/patricia/bytebank/model/Endereco.kt
@@ -9,4 +9,13 @@ class Endereco(
     var cep: String = "",
     var complemento: String = ""
 ){
+    override fun toString(): String {
+        return """Endereco(logradouro='$logradouro', 
+            |numero=$numero, 
+            |bairro='$bairro', 
+            |cidade='$cidade', 
+            |estado='$estado', 
+            |cep='$cep', 
+            |complemento='$complemento')""".trimMargin()
+    }
 }
