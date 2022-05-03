6d48c8962a5c282f60c0be5ab671f2acc91a5ed2
pmagalhaes2 <paath.magalhaes2@gmail.com>
2022-05-03 18:08:52 -0300

    Sobrescrevendo o método equals e hashCode

--- pacotes-e-composicao/src/br/com/patricia/bytebank/main.kt
+++ pacotes-e-composicao/src/br/com/patricia/bytebank/main.kt
@@ -1,8 +1,17 @@
 import br.com.patricia.bytebank.model.Endereco
 
 fun main() {
-    val endereco = Endereco(logradouro = "Rua Vergueiro", numero = 1000)
-    val enderecoNovo = Endereco(bairro = "Santa Cruz")
+    val endereco = Endereco(
+        logradouro = "Rua Vergueiro",
+        numero = 1000,
+        cep = "00000-000"
+    )
+    val enderecoNovo = Endereco(
+    logradouro = "Rua Vergueiro",
+    numero = 1000,
+    cep = "00000-000"
+    )
+
     println(endereco.equals(enderecoNovo))
 
     println(endereco.hashCode())
